dfecce914a9a033567e447d149a5a1a240d3fc3d
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-24 23:28:40 -0300

    se declaran los atributos de la clase Producto

--- tp2_MartinezRobertoAriel/src/ar/edu/unju/fi/model/Producto.java
+++ tp2_MartinezRobertoAriel/src/ar/edu/unju/fi/model/Producto.java
@@ -1,5 +1,19 @@
 package ar.edu.unju.fi.model;
 
 public class Producto {
+	
+	int codigo;
+	String descripcion;
+	Double precioUnitario;
+	
+	enum OrigenFabricacion{
+		ARGENTINA, CHINA, BRASIL, URUGUAY
+	}
+	
+	enum Categoria{
+		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
+	}
+	
+	
 
 }
