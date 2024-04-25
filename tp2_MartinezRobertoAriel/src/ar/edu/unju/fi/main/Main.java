4bb6d4cb00dda54ca78da128a756c18463e63445
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-25 00:32:17 -0300

    agregando funciones Menu y crearProducto en main

--- tp2_MartinezRobertoAriel/src/ar/edu/unju/fi/main/Main.java
+++ tp2_MartinezRobertoAriel/src/ar/edu/unju/fi/main/Main.java
@@ -1,10 +1,80 @@
 package ar.edu.unju.fi.main;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import ar.edu.unju.fi.model.Producto;
+
 public class Main {
+	
+	static Scanner scanner;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// desarrollo
+		
+		scanner = new Scanner(System.in);
+				
+		ArrayList<Producto> listaProductos = new ArrayList<>();
+		
+		
+		
+		int opc=0;
+		
+		
+		
+		
+		do {
+			
+			menu(opc);
+			
+			switch (opc) {
+			
+			case 1: crearProducto(listaProductos);  
+					break;
+				
+				
+			case 4: System.out.println("Hasta luego");
+					break;
+			
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + opc);
+			}
+			
+			
+			
+			
+			
+		}while(opc!= 4);
+		
+		
+		
 
+	}
+	
+	
+	public static void menu(int opc) {
+		
+		System.out.println("*********   MENU PRINCIPAL           **********");
+		System.out.println("*********   1 - Crear Producto       **********");
+		System.out.println("*********   2 - Mostrar Productos    **********");
+		System.out.println("*********   3 - Modificar Producto   **********");
+		System.out.println("*********   4 - Salir                **********");
+		System.out.println("");
+		
+		System.out.println(opc);		
+		
+	}
+	
+	
+	public static void crearProducto(ArrayList<Producto> listaProductos) {
+		
+		Producto producto = new Producto();
+		
+		System.out.println("-------  Origen de Fabricacion  --------");
+		
+		for(Producto.OrigenFabricacion origen : Producto.OrigenFabricacion.values()) {}
+		
+		
 	}
 
 }
--- tp2_MartinezRobertoAriel/src/ar/edu/unju/fi/model/Producto.java
+++ tp2_MartinezRobertoAriel/src/ar/edu/unju/fi/model/Producto.java
@@ -5,15 +5,77 @@ public class Producto {
 	int codigo;
 	String descripcion;
 	Double precioUnitario;
+	OrigenFabricacion origen;
+	Categoria categoria;
 	
-	enum OrigenFabricacion{
+	public enum OrigenFabricacion{
 		ARGENTINA, CHINA, BRASIL, URUGUAY
 	}
 	
-	enum Categoria{
+	public enum Categoria{
 		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
 	}
 	
+	public Producto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Producto(int codigo, String descripcion, Double precioUnitario, OrigenFabricacion origen,
+			Categoria categoria) {
+		super();
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precioUnitario = precioUnitario;
+		this.origen = origen;
+		this.categoria = categoria;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(Double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public OrigenFabricacion getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(OrigenFabricacion origen) {
+		this.origen = origen;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [codigo=" + codigo + ", descripcion=" + descripcion + ", precioUnitario=" + precioUnitario
+				+ ", origen=" + origen + ", categoria=" + categoria + "]";
+	}
+
 	
 
 }
