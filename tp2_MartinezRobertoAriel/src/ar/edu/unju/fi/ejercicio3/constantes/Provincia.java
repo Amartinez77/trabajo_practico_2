a82b7d65b3d092aaafab2c1a0df461a97b50c439
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-27 02:09:27 -0300

    agregando ejercicio 3 - terminado

--- tp2_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio3/constantes/Provincia.java
+++ tp2_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio3/constantes/Provincia.java
@@ -1,5 +1,46 @@
 package ar.edu.unju.fi.ejercicio3.constantes;
 
 public enum Provincia {
+	
+	JUJUY, SALTA, TUCUMAN, CATAMARCA, LA_RIOJA, SANTIAGO_DEL_ESTERO;
+	
+	int cantidadPoblacion;
+	Double superficie;
+	
+	private Provincia() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private Provincia(int cantidadPoblacion, Double superficie) {
+		this.cantidadPoblacion = cantidadPoblacion;
+		this.superficie = superficie;
+	}
+
+
+	public int getCantidadPoblacion() {
+		return cantidadPoblacion;
+	}
+
+
+	public void setCantidadPoblacion(int cantidadPoblacion) {
+		this.cantidadPoblacion = cantidadPoblacion;
+	}
+
+
+	public Double getSuperficie() {
+		return superficie;
+	}
+
+
+	public void setSuperficie(Double superficie) {
+		this.superficie = superficie;
+	}
+	
+	 // Método para calcular la densidad poblacional
+    public double densidadPoblacional() {
+        return (double) cantidadPoblacion / superficie;
+    }
+	
+	
 
 }
