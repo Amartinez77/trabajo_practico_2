74b1099efde671b6285a56af92b5016bd0ef08b5
Ariel Martinez <ariel.mtz85@gmail.com>
2024-04-29 20:45:28 -0300

    agregando clases pagoEfectivo y pagoTarjeta

--- tp2_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
+++ tp2_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
@@ -1,8 +1,52 @@
 package ar.edu.unju.fi.ejercicio5.model;
 
+import java.time.LocalDate;
+
 import ar.edu.unju.fi.ejercicio5.interfaces.Pago;
 
 public class PagoEfectivo implements Pago {
+	
+	private double montoPagado;
+	 private LocalDate fechaPago;
+
+	public PagoEfectivo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public PagoEfectivo(double montoPagado, LocalDate fechaPago) {
+		super();
+		this.montoPagado = montoPagado;
+		this.fechaPago = fechaPago;
+	}
+	
+
+	public double getMontoPagado() {
+		return montoPagado;
+	}
+
+
+	public void setMontoPagado(double montoPagado) {
+		this.montoPagado = montoPagado;
+	}
+
+
+
+
+	public LocalDate getFechaPago() {
+		return fechaPago;
+	}
+
+
+
+
+	public void setFechaPago(LocalDate fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+
+
+
 
 	@Override
 	public void realizarPago(Double monto) {
--- tp2_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio5/model/PagoTarjeta.java
+++ tp2_MartinezRobertoAriel/src/ar/edu/unju/fi/ejercicio5/model/PagoTarjeta.java
@@ -9,6 +9,55 @@ public class PagoTarjeta implements Pago {
 	private String numeroTarjeta;
     private LocalDate fechaPago;
     private double montoPagado;
+    
+    
+    public PagoTarjeta() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PagoTarjeta(String numeroTarjeta, LocalDate fechaPago, double montoPagado) {
+		super();
+		this.numeroTarjeta = numeroTarjeta;
+		this.fechaPago = fechaPago;
+		this.montoPagado = montoPagado;
+	}
+	
+	
+
+	public String getNumeroTarjeta() {
+		return numeroTarjeta;
+	}
+
+
+
+	public void setNumeroTarjeta(String numeroTarjeta) {
+		this.numeroTarjeta = numeroTarjeta;
+	}
+
+
+
+	public LocalDate getFechaPago() {
+		return fechaPago;
+	}
+
+
+
+	public void setFechaPago(LocalDate fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+
+
+
+	public double getMontoPagado() {
+		return montoPagado;
+	}
+
+
+
+	public void setMontoPagado(double montoPagado) {
+		this.montoPagado = montoPagado;
+	}
+
 
 
 	@Override
